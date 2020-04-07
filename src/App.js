089e3cf3e50fa04753e5751a4ab7405ca47deb8c
timoduethorn <timo.duethorn@myrepublic.net>
2020-04-07 11:49:48 +0800

    Updated App & Index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ApolloClient from "apollo-boost";
-
-//import { ApolloProvider } from "react-apollo";
-import { ApolloProvider } from '@apollo/react-hooks';
-import { useQuery } from '@apollo/react-hooks';
-import { render } from 'react-dom';
-// import gql from 'graphql-tag';
-import { gql } from "apollo-boost";
 
 function App() {
   return (
@@ -18,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
           Learn React
         </a>
       </header>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { gql } from "apollo-boost";
 import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
+
+import { useQuery } from '@apollo/react-hooks';
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:5000/graphql',
+// });
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'https://48p1r2roz4.sse.codesandbox.io'
 });
 
 client
@@ -23,12 +33,50 @@ client
 })
 .then(result => console.log(result));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const EXCHANGE_RATES = gql`
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+const GET_TILES = gql`
+{
+  getTiles {
+    data {
+      id
+    }
+  }
+}
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
+    </div>
+  </ApolloProvider>
 );
+
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
